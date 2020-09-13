4dab0475a177f301c7e0d1ac00b2c2eadd96243b
mirmahyar <m.mirsamadi@gmail.com>
2020-09-14 00:18:20 +0430

    more routing

--- main.go
+++ main.go
@@ -6,22 +6,33 @@ import (
 	"net/http"
 )
 
-func homepage(w http.ResponseWriter, h *http.Request) {
+func homepage(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("hello kir"))
 
 }
 
-func showSnippet(w http.ResponseWriter, h *http.Request) {
+func showSnippet(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a specific snippet"))
 
 }
 
-func createSnippet(w http.ResponseWriter, h *http.Request) {
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
 	w.Write([]byte("create a snippet"))
 
 }
 
 func main() {
+
+	//For security reasons, we don't use Handlefunc directly, but we define a mux and use it.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", homepage)
 	mux.HandleFunc("/snippet", showSnippet)
